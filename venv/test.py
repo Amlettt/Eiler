ce4a4be2b38955cfe7c783f7595982d09e9a60a4
Chepik.Mikhail <mixapank-rocker@mail.ru>
2020-02-13 18:07:06 +0300

    new task

--- venv/test.py
+++ venv/test.py
@@ -1,2 +1,17 @@
+s = 'dfgdf', 'dfsd'
+m = (1, 2)
+h = [1, 2]
+g = {1: 2, 2: 3}
+d = ''
+a = 'Mama'
+d = a
 
-print(30%7)
+print('ma' in a)
+print('{0} {1}'.format(g[2], g[1]))
+say = {'lala', 'daddy', 'hello', 'mum'}
+words = {'hello', 'daddy', 'hello', 'mum'}
+print(say - words)
+
+delimiter = '_*_'
+mylist = ['Бразилия', 'Россия', 'Индия', 'Китай']
+print(delimiter.join(mylist))
